1ce6a182f6dc30bca9fd55c79e937d0168399fa9
Mariya Sharova <marii.savit@gmail.com>
2022-11-07 22:22:10 +0300

    add fibo

---
+++ homework_2.2/script.js
@@ -0,0 +1,30 @@
+function fibo () {}
+
+const resultF = (() => {
+    {
+        let a  = 0;
+        let b = 1;
+        let counter = 0;
+
+        return () => {
+            if (counter === 0) {
+                counter++;
+                return 1;
+            }
+            if (counter === 2) {
+                counter++;
+                return 2;
+            }
+            let result = a + b;
+            a = b;
+            b = result;
+            return result; 
+        }
+    }
+})();
+
+console.log(resultF());
+console.log(resultF());
+console.log(resultF());
+console.log(resultF());
+console.log(resultF());
